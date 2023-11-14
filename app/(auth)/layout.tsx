1cf2906695b272017b763b56cc90faabc12e8475
hiepnn <hiepnn@vnpay.vn>
2023-11-14 17:26:46 +0700

    init keycloak

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,12 +1,39 @@
+
+"use client";
+import Keycloak from 'keycloak-js';
+const keycloak = new Keycloak({
+    realm: 'porfolio',
+    url: 'http://keycloak.com.au:8080${kc_base_path}',
+    clientId: 'porfolio',
+});
+const initKeycloak = async ()=>{
+    try {
+        const authenticated = await keycloak.init({
+            onLoad: 'login-required'
+        });
+        console.log(`User is ${authenticated ? 'authenticated' : 'not authenticated'}`);
+    } catch (error) {
+        console.error('Failed to initialize adapter:', error);
+    }
+}
+
 const AdminLayout = (props: {
     children: React.ReactNode;
 }) => {
-    return <div>
-        <header>Header</header>
-        <aside>Sidebar</aside>
-        {props.children}
-        <footer>Footer</footer>
-    </div>
+    initKeycloak();
+    return (
+        <html lang="en" className="!scroll-smooth">
+        <body>
+            <div>
+                <header>Header</header>
+                <aside>Sidebar</aside>
+                {props.children}
+                <footer>Footer</footer>
+            </div>
+        </body>
+    </html>
+    )
+
 }
 
 
