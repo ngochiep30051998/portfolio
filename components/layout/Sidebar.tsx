7bdfbb67834a172f3fece0f500b05a851cfb4b59
hiepnn <hiepnn@vnpay.vn>
2023-11-20 14:49:46 +0700

    layout

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,77 +1,20 @@
 import React, { useEffect, useRef, useState } from "react";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
-import SidebarLinkGroup from "./SidebarLinkGroup";
-import Image from "next/image";
-import { cn } from "@/lib/utils";
-import MiniSidebar from "./MiniSidebar";
-import SidebarExpanded from "./SidebarExpanded";
+import { HomeIcon } from '@heroicons/react/20/solid'
 
-interface SidebarProps {
-    sidebarOpen?: boolean;
-    setSidebarOpen: (arg: boolean) => void;
-}
 
-const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
-    const pathname = usePathname();
+const Sidebar = () => {
 
-    const trigger = useRef<any>(null);
-    const sidebar = useRef<any>(null);
-
-    let storedSidebarExpanded = "true";
-    const [sidebarExpanded, setSidebarExpanded] = useState(
-        storedSidebarExpanded === null ? false : storedSidebarExpanded === "true"
-    );
-
-    // close on click outside
-    useEffect(() => {
-        const clickHandler = ({ target }: MouseEvent) => {
-            if (!sidebar.current || !trigger.current) return;
-            if (
-                !sidebarOpen ||
-                sidebar.current.contains(target) ||
-                trigger.current.contains(target)
-            )
-                return;
-            setSidebarOpen(false);
-        };
-        document.addEventListener("click", clickHandler);
-        return () => document.removeEventListener("click", clickHandler);
-    });
-
-    // close if the esc key is pressed
-    useEffect(() => {
-        const keyHandler = ({ keyCode }: KeyboardEvent) => {
-            if (!sidebarOpen || keyCode !== 27) return;
-            setSidebarOpen(false);
-        };
-        document.addEventListener("keydown", keyHandler);
-        return () => document.removeEventListener("keydown", keyHandler);
-    });
-
-    useEffect(() => {
-        localStorage.setItem("sidebar-expanded", sidebarExpanded.toString());
-        if (sidebarExpanded) {
-            document.querySelector("body")?.classList.add("sidebar-expanded");
-        } else {
-            document.querySelector("body")?.classList.remove("sidebar-expanded");
-        }
-    }, [sidebarExpanded]);
-
-    const handleSidebar = () => {
-        setSidebarExpanded(curr => !curr)
-    }
     return (
         <div id="application-sidebar" className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 start-0 bottom-0 z-[60] w-64 bg-white border-e border-gray-200 pt-7 pb-10 overflow-y-auto lg:block lg:translate-x-0 lg:end-auto lg:bottom-0 [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-slate-700 dark:[&::-webkit-scrollbar-thumb]:bg-slate-500 dark:bg-gray-800 dark:border-gray-700">
             <div className="px-6">
-                <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Brand</a>
+                <a className="flex-none text-xl font-semibold dark:text-white" href="#" aria-label="Brand">Portfolio Admin</a>
             </div>
 
             <nav className="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
                 <ul className="space-y-1.5">
                     <li>
                         <a className="flex items-center gap-x-3.5 py-2 px-2.5 bg-gray-100 text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-900 dark:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600" href="#">
-                            <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" ><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline points="9 22 9 12 15 12 15 22" /></svg>
+                            <HomeIcon className="w-4 h-4"/>
                             Dashboard
                         </a>
                     </li>
